792d85f15c3dd5bd3b9429cd5e846299114326ed
Fabio Tavoloni <fabio.t0844.beyond@xpi.com.br>
2018-06-22 08:55:17 -0300

    insertindo scaffolding via Visual Studio 2017 (porque VSCode no Linux não funcionou)

--- 0-Presentation/ProjetoModeloDDD.MVC/Controllers/ClientesController.cs
+++ 0-Presentation/ProjetoModeloDDD.MVC/Controllers/ClientesController.cs
@@ -1,15 +1,11 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoModeloDDD.Domain.Entities;
 using ProjetoModeloDDD.Infra.Data.Repositories;
-using ProjetoModeloDDD.MVC.Models;
 using ProjetoModeloDDD.MVC.ViewModels;
-
 
 namespace ProjetoModeloDDD.MVC.Controllers
 {
@@ -17,23 +13,95 @@ namespace ProjetoModeloDDD.MVC.Controllers
     {
         private readonly ClienteRepository _clienteRepository = new ClienteRepository();
 
+        // GET: Clientes
         public ActionResult Index()
         {
             var clienteViewModel = Mapper.Map<IEnumerable<Cliente>, IEnumerable<ClienteViewModel>>(_clienteRepository.GetAll());
             return View(clienteViewModel);
         }
 
+        // GET: Clientes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(ClienteViewModel cliente){
-            
-            if (!ModelState.IsValid)
+        public ActionResult Create(ClienteViewModel cliente)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    var clienteDomain = Mapper.Map<ClienteViewModel, Cliente>(cliente);
+                    _clienteRepository.Add(clienteDomain);
+
+                    return RedirectToAction("Index");
+                }
+
                 return View(cliente);
 
-            var clienteDomain = Mapper.Map<ClienteViewModel, Cliente>(cliente);
-            _clienteRepository.Add(clienteDomain);
+                //return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
 
-            return RedirectToAction("Index");
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Clientes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
